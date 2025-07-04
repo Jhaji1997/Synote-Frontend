181c377d46a081927d856c3df99498332b739a59
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-04 21:44:23 +0530

    Add error handling in authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,31 +1,55 @@
 import axios from "../lib/axios.js";
 
 export const registerUser = async (formData) => {
-  const response = await axios.post("/users/register", formData);
-  return response.data;
+  try {
+    const response = await axios.post("/users/register", formData);
+    return response.data;
+  } catch (err) {
+    throw err?.response?.data || { message: "Error registering user" };
+  }
 };
 
 export const loginUser = async (formData) => {
-  const response = await axios.post("/users/login", formData);
-  return response.data;
+  try {
+    const response = await axios.post("/users/login", formData);
+    return response.data;
+  } catch (err) {
+    throw err?.response?.data || { message: "Error logging in" };
+  }
 };
 
 export const logoutUser = async () => {
-  const response = await axios.post("/users/logout");
-  return response.data;
+  try {
+    const response = await axios.post("/users/logout");
+    return response.data;
+  } catch (err) {
+    throw err?.response?.data || { message: "Error logging out" };
+  }
 };
 
 export const refreshAccessToken = async () => {
-  const response = await axios.post("/users/refresh-tokens");
-  return response.data;
+  try {
+    const response = await axios.post("/users/refresh-tokens");
+    return response.data;
+  } catch (err) {
+    throw err?.response?.data || { message: "Error refreshing token" };
+  }
 };
 
 export const getCurrentUser = async () => {
-  const response = await axios.get("/users/me");
-  return response.data;
+  try {
+    const response = await axios.get("/users/me");
+    return response.data;
+  } catch (err) {
+    throw err?.response?.data || { message: "Error fetching current user" };
+  }
 };
 
 export const updateAvatar = async (avatarImage) => {
-  const response = await axios.patch("/users/me",{avatarImage});
-  return response.data;
+  try {
+    const response = await axios.patch("/users/me", { avatarImage });
+    return response.data;
+  } catch (err) {
+    throw err?.response?.data || { message: "Error updating avatar" };
+  }
 };
